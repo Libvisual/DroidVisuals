981e47d0f7f0b5d8a83d75dcee0456f4aa617c2a
Scott Sibley <sisibley@gmail.com>
2012-08-10 02:20:08 -0500

    Add -fstrict-aliasing.

--- DroidVisuals/jni/Application.mk
+++ DroidVisuals/jni/Application.mk
@@ -4,5 +4,5 @@ APP_STL := gnustl_shared
 #STLPORT_FORCE_REBUILD := true
 APP_OPTIM := $(BUILD_TYPE)
 APP_CPPFLAGS := -std=gnu++0x 
-APP_CFLAGS := -Iinclude/ -Wno-psabi -DBUILD_GLU32 -DUSE_OPENGL_ES 
+APP_CFLAGS := -Iinclude/ -Wno-psabi -DBUILD_GLU32 -DUSE_OPENGL_ES -fstrict-aliasing
 APP_CFLAGS += -D_GLIBCXX_USE_C99_STDINT_TR1 -DVISUAL_OS_ANDROID
