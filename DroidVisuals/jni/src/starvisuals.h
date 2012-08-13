6af7a7e7a0fd20a6040f17a5ac118195e3d747b0
Scott Sibley <sisibley@gmail.com>
2012-08-13 00:13:47 -0500

    (DroidVisuals) Add a make file for gforce. Remove luawrapper as it wasn't being used.

--- DroidVisuals/jni/src/starvisuals.h
+++ DroidVisuals/jni/src/starvisuals.h
@@ -36,7 +36,7 @@
 
 // Initial plugins. Preferences should override these.
 #define MORPH "tentacle"
-#define ACTOR "lv_scope"
+#define ACTOR "lv_analyzer"
 #define INPUT "debug"
 
 #define URL_GPLv2 "http://www.gnu.org/licenses/gpl-2.0.txt"
